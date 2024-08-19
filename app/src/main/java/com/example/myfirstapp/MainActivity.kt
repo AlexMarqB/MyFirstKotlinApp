8bcd04b5d71223f3e2b2abb683dbdeb3b312e420
Alex Marques Beraldo <alexmarquesberaldo@gmail.com>
2024-08-19 09:50:42 -0300

    theory comments

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -12,14 +12,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myfirstapp.ui.theme.MyFirstAppTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+//Starting point for our app
+class MainActivity : /*Behave like ->*/ ComponentActivity() {
+    //Overrides the default function "onCreate"
+    override fun onCreate(savedInstanceState: Bundle? /*Settings*/) {
+        //Execute the default onCreate and than execute what we write
         super.onCreate(savedInstanceState)
         setContent {
             MyFirstAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize() /*This surface must occupy the whole screen*/,
+                    color = MaterialTheme.colorScheme.background /*Use the colors from the theme*/) {
+                    //Composable or component => Are elements that we can see on the screen
+                    Greeting("Alex") //Text that appears on the screen from the component
                 }
             }
         }
@@ -29,15 +34,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
+            text = "Welcome $name!",
             modifier = modifier
     )
 }
 
+//Quick testing without restarting the whole app
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyFirstAppTheme {
-        Greeting("Android")
+        Greeting("Alex")
     }
 }
